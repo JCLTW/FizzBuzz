e2194092f5f113254b72473aa463472d71c624b0
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-13 00:06:31 +0800

    feat, multiple_of_3

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -6,6 +6,11 @@ namespace FizzBuzz
     {
         public static string CountOff(int orderNumber)
         {
+            if (orderNumber % 3 == 0)
+            {
+                return "Fizz";
+            }
+
             return orderNumber.ToString();
         }
     }
