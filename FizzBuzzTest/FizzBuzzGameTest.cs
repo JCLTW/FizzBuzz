9419fc4c890c4d751542d734d9334d20aee8d4cf
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-12 23:21:48 +0800

    test, tasking case for the given number multiple one of the 3, 5 or 7

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -16,5 +16,38 @@ namespace FizzBuzzTest
             // Then
             Assert.Equal(givenNumber.ToString(), result);
         }
+
+        [Fact]
+        public void Should_return_the_Fizz_when_countoff_given_a_number_not_multiple_of_3()
+        {
+            // Given
+            int givenNumber = 3;
+            // When
+            string result = FizzBuzzGame.CountOff(givenNumber);
+            // Then
+            Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void Should_return_the_Buzz_when_countoff_given_a_number_not_multiple_of_5()
+        {
+            // Given
+            int givenNumber = 5;
+            // When
+            string result = FizzBuzzGame.CountOff(givenNumber);
+            // Then
+            Assert.Equal("Buzz", result);
+        }
+
+        [Fact]
+        public void Should_return_the_Whizz_when_countoff_given_a_number_not_multiple_of_7()
+        {
+            // Given
+            int givenNumber = 7;
+            // When
+            string result = FizzBuzzGame.CountOff(givenNumber);
+            // Then
+            Assert.Equal("Whizz", result);
+        }
     }
 }
