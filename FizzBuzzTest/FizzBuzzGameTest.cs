55c7626c35718e2af49f7adf338feb29df43412c
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-12 23:26:36 +0800

    test: enrich the cases by paramatized given number

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -6,44 +6,49 @@ namespace FizzBuzzTest
 {
     public class FizzBuzzGameTest
     {
-        [Fact]
-        public void Should_return_the_given_number_when_countoff_given_a_number_not_multiple_of_3_5_7()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        public void Should_return_the_given_number_when_countoff_given_a_number_not_multiple_of_3_5_7(int givenNumber)
         {
-            // Given
-            int givenNumber = 1;
             // When
             string result = FizzBuzzGame.CountOff(givenNumber);
             // Then
             Assert.Equal(givenNumber.ToString(), result);
         }
 
-        [Fact]
-        public void Should_return_the_Fizz_when_countoff_given_a_number_not_multiple_of_3()
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(9)]
+        public void Should_return_the_Fizz_when_countoff_given_a_number_not_multiple_of_3(int givenNumber)
         {
-            // Given
-            int givenNumber = 3;
             // When
             string result = FizzBuzzGame.CountOff(givenNumber);
             // Then
             Assert.Equal("Fizz", result);
         }
 
-        [Fact]
-        public void Should_return_the_Buzz_when_countoff_given_a_number_not_multiple_of_5()
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(20)]
+        public void Should_return_the_Buzz_when_countoff_given_a_number_not_multiple_of_5(int givenNumber)
         {
-            // Given
-            int givenNumber = 5;
             // When
             string result = FizzBuzzGame.CountOff(givenNumber);
             // Then
             Assert.Equal("Buzz", result);
         }
 
-        [Fact]
-        public void Should_return_the_Whizz_when_countoff_given_a_number_not_multiple_of_7()
+        [Theory]
+        [InlineData(7)]
+        [InlineData(14)]
+        [InlineData(21)]
+        public void Should_return_the_Whizz_when_countoff_given_a_number_not_multiple_of_7(int givenNumber)
         {
-            // Given
-            int givenNumber = 7;
             // When
             string result = FizzBuzzGame.CountOff(givenNumber);
             // Then
